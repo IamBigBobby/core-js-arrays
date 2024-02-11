5e0624c9a24fff1c8bf1a1da920608f168471e2d
Denis Svetleishii <denissvetleishii@outlook.com>
2024-02-11 12:24:47 +0400

    feat: add solution distinct

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -290,8 +290,9 @@ function toStringList(arr) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const result = Array.from(new Set(arr));
+  return result;
 }
 
 /**
